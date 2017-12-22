08734de23b905eb1f82cbfec5ed9e9c553e4c43a
Birte Goldt <bigo@akra.de>
2017-12-22 17:50:29 +0100

    add authors show view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :posts, only: [:index, :show]
+  resources :authors, only: [:show]
   root to: 'posts#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
